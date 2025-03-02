cbf908c76fb39dca3a0197a4923d9e2d273b51a1
Naincy-10264 <naincykatiyar09@gmail.com>
2025-03-03 02:58:41 +0530

    files added

--- Java_Script/index.js
+++ Java_Script/index.js
@@ -1,2 +1,15 @@
-var arr=["Naincy", "Abhishek", "Akshat"];
-console.log(arr);
+// const prompt = require("prompt-sync")();
+
+// var arr=["Naincy", "Abhishek", "Akshat"];
+// console.log(arr);
+// console.log(arr[1]);
+// console.log(arr.length);
+// var name=prompt("enter your name ");
+// if(arr.includes(name)){
+//     console.log("True! your name is there");
+// }
+// else{
+//     console.log("Lets! try next time");
+// }
+
+
