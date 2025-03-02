c3c25e01ecbdec7bb4a65a0f142dcf41e5994662
Naincy-10264 <naincykatiyar09@gmail.com>
2025-03-03 02:59:54 +0530

    modified

--- Java_Script/index.js
+++ Java_Script/index.js
@@ -1,15 +1,15 @@
-// const prompt = require("prompt-sync")();
+const prompt = require("prompt-sync")();
 
-// var arr=["Naincy", "Abhishek", "Akshat"];
-// console.log(arr);
-// console.log(arr[1]);
-// console.log(arr.length);
-// var name=prompt("enter your name ");
-// if(arr.includes(name)){
-//     console.log("True! your name is there");
-// }
-// else{
-//     console.log("Lets! try next time");
-// }
+var arr=["Naincy", "Abhishek", "Akshat"];
+console.log(arr);
+console.log(arr[1]);
+console.log(arr.length);
+var name=prompt("enter your name ");
+if(arr.includes(name)){
+    console.log("True! your name is there");
+}
+else{
+    console.log("Lets! try next time");
+}
 
 
