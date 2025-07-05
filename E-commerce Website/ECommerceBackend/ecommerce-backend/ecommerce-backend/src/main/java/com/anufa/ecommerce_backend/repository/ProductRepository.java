b4a2c865f5a232fd741fa927c298689e9a3dc3f9
Naincy-10264 <naincykatiyar09@gmail.com>
2025-07-06 00:24:57 +0530

    deleted backened and style.css

--- E-commerce Website/ECommerceBackend/ecommerce-backend/ecommerce-backend/src/main/java/com/anufa/ecommerce_backend/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.anufa.ecommerce_backend.repository;
-
-import com.anufa.ecommerce_backend.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
